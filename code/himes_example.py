9cae1d6b422d9c8c34e3f0c45daad90eceb08131
ArtsyAnt <antonio.mariomd@gmail.com>
2026-06-16 12:01:02 -0600

    Feat: added a fermi catalog parsing script, global_indexing function to merge spectral windows, and furthered fermi constraints on himes script.

--- code/himes_example.py
+++ code/himes_example.py
@@ -1,15 +1,19 @@
 # example of Fermi Unassociated Sources in the MeerKAT Absorption Line Survey catalog reduction using classes
-
+# this should use the 3rd data release which is v31 not v35
 from multiwavecataloging import Catalog
 from multiwavecataloging import CatalogOverlayer
 from multiwavecataloging import fermi_plot
+from multiwavecataloging import spectral_window_merging
+from multiwavecataloging import global_index
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 import astropy.coordinates as coord
 from astropy.io import fits
 from astropy.table import vstack
-from astropy.table import Table
+from astropy.table import Table, Column, MaskedColumn
+# from astropy.table import vstack
+
 from astropy import wcs
 from regions import EllipseSkyRegion
 
@@ -22,9 +26,6 @@ from matplotlib.patches import Ellipse as ellipse
 
 # Data handling
 import numpy as np
-import specutils.analysis as spec_ana
-from scipy.optimize import curve_fit
-from specutils import SpectralRegion, Spectrum1D
 
 
 def main():    
@@ -32,50 +33,81 @@ def main():
     vizier = Vizier(columns=[])
     vizier.ROW_LIMIT = -1# -1 means unlimited
     
+    '''CATALOG CALLS'''
     mals_catalog = vizier.get_catalogs('J/ApJS/270/33')[0]
-    Fermi_hud = fits.open('/Users/mario/Coding/nrao_reu_research/socorro/USSCataloging/catalogs/gll_psc_v35.fit')
+    # changed fermi data call - 6/12
+    Fermi_hud = fits.open('/Users/mario/Coding/nrao_reu_research/socorro/USSCataloging/catalogs/gll_psc_v31.fit')
     Fermi_catalog = Table(Fermi_hud[1].data)
     
-    # reordering 
+    # make sure fermi is only calling unassociated sources
+    all_associations_types = list(set(Fermi_catalog['CLASS1']))
+    unknown_associated_sources = [class_type for class_type in all_associations_types if class_type.islower()]
+
+    class_type_mask = np.isin(Fermi_catalog['CLASS1'], unknown_associated_sources)
+    Fermi_catalog = Fermi_catalog[class_type_mask]
+
+    # reordering (want to put this into)
     # weird its double counting the catalog so you have to make a copy...
-    mals_reordered = mals_catalog['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit']
-    # mals_reordered_2 = mals_catalog['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit']
+    mals_reordered = mals_catalog['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit', 'e_Spwfit', 'Scode', 'SPW/WB']
     fermi_reordered = Fermi_catalog['RAJ2000', 'DEJ2000','Conf_95_SemiMajor', 'Conf_95_SemiMinor', 'Conf_95_PosAng']
 
-
+    '''FERMI OVERLAY'''
     # i would call in this into the catalog overlay not anything else
-    mals_cataloging_uss = Catalog(mals_reordered, ['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit'], catalog_type='uss')
+    mals_cataloging_uss = Catalog(mals_reordered, ['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit', 'e_Spwfit',  'Scode', 'SPW/WB'], catalog_type='uss')
     # mals_cataloging_css = Catalog(mals_reordered_2, ['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit'], catalog_type='css')
     fermi_cataloging = Catalog(fermi_reordered,['RAJ2000', 'DEJ2000','Conf_95_SemiMajor', 'Conf_95_SemiMinor', 'Conf_95_PosAng'], catalog_type='xray' )
     # since this is forced to be in a [] maybe fix it for single iteration if the user decides not to put it into a ls format?
     fermi_mals_overlay = CatalogOverlayer(base_catalog=fermi_cataloging, comparision_catalogs=[mals_cataloging_uss])
 
-    # this is a temp solution, we would rather have the indexing values be the catalog's 'ID' values 
-    # so that we dont have to revert back to calling the semi reduced catalog (uss-canidate in this case) rather than just call 
-    # the exampled mal_cat_final. Also would clear clutter.
     fermi_ellipses_final, mal_cat_final, dict_mal_femi, uss_candidate_catalog  = fermi_mals_overlay.matched_catalogs()
-
     fermi_ellipses_final = fermi_ellipses_final[0]
-    
     mal_cat_final = mal_cat_final[0]
     # ls all catalogs without the matching
     # dict key
     dict_mal_femi = dict_mal_femi[0]
-    dict_values = [*dict_mal_femi.values()]
-    dict_keys = list(dict_mal_femi.keys())
+    # dict_values = [*dict_mal_femi.values()]
+    # dict_keys = list(dict_mal_femi.keys())
+ 
+    '''SPW CROSSMATCHING'''
+    spw2_catalog = mal_cat_final[mal_cat_final['SPW/WB'] == 'LSPW_2']
+    spw9_catalog = mal_cat_final[mal_cat_final['SPW/WB'] == 'LSPW_9']
 
+    spw2_cataloging = Catalog(spw2_catalog, ['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit', 'Scode', 'SPW/WB'])
+    spw9_cataloging = Catalog(spw9_catalog, ['RAJ2000', 'DEJ2000', 'Majaxis', 'Minaxis', 'PA', 'Flux', 'FluxPk', 'Spwfit', 'Scode', 'SPW/WB'])
+    spw2_spw9_overlay = CatalogOverlayer(base_catalog=spw2_cataloging, comparision_catalogs=[spw9_cataloging], beam_scale=2)
+
+    # this will return only the matched sources while the mal_cat_final wil have the indexes of all sources,
+    # so the indexes in mal_cat not in the spw2_9 match are the ones of interest 
+    spw2_matched_catalog, spw9_matched_catalog, dict_spw2_sp9, spw9_all_candidates = spw2_spw9_overlay.matched_catalogs()
+    spw2_matched_catalog = spw2_matched_catalog[0]
+    spw9_matched_catalog = spw9_matched_catalog[0]
+    dict_spw2_sp9 = dict_spw2_sp9[0]
+    spw9_all_candidates = spw9_all_candidates[0]
+
+    print(f'All Sources {len(mal_cat_final)}')
+    print(f'Cross Spectral Window Matches {len(dict_spw2_sp9.keys())}')
+    
+    # combines nearby sources across spectral bands
+    unique_sources_sp2 = spectral_window_merging(spw2_catalog, spw9_catalog, dict_spw2_sp9 )
+    dict_mal_femi_global_index = global_index(dict_mal_femi, uss_candidate_catalog)
+    dict_mal_femi_updated = {key: [value for value in value_list if value in (unique_sources_sp2['Indexes'])] 
+                            for key, value_list in dict_mal_femi_global_index.items()
+                             }
+    dict_values = [*dict_mal_femi_updated.values()]
+    dict_keys = list(dict_mal_femi_updated.keys())
+    
+    '''FERMI PLOTTING'''
     # defining the ellipse values
     # change this to a function in the future
     ra_fermi = coord.Angle(fermi_ellipses_final['RAJ2000']*u.deg)
     ra_fermi = - ra_fermi
     ra_reordered_fermi = ra_fermi.wrap_at(180*u.deg)
     dec_fermi = coord.Angle(fermi_ellipses_final['DEJ2000']*u.deg)
-
-    
     semi_major_ax = fermi_ellipses_final['Conf_95_SemiMajor']*u.deg
     semi_minor_ax = fermi_ellipses_final['Conf_95_SemiMinor']*u.deg
     position_angle = fermi_ellipses_final['Conf_95_PosAng']*u.deg
     
+    # makes fermi patches
     fermi_ellipses_patches = []
     for i in range(len(fermi_ellipses_final['RAJ2000'])):
         fermi_ellipses_patch = ellipse((ra_reordered_fermi[i].value, dec_fermi[i].value), semi_major_ax[i].value*2, semi_minor_ax[i].value*2, angle=position_angle[i].value, lw=2, edgecolor='b', alpha=0.5, zorder=1)
@@ -83,8 +115,15 @@ def main():
     # will save figure of a plt figure
     # plotting the first iteration of the ferm ellipses overlap THIS IS VARIABLE change if desired
     
-    # input the lens to show max someone would want to change? print(input(len...))
-    fermi_plot(fermi_ellipses_patches[0], dict_keys[0], uss_candidate_catalog, dict_values[0])
+    # ask for an input of how many sources lens to show max someone would want to change? print(input(len...))
+    # print(f'ALL USS sources within Fermi Ellipses: {len(mal_cat_final)}')
+    print(f'Unique USS sources within Fermi Ellipses: {len(unique_sources_sp2)}')
+    print(f'Fermi Ellipses: {len(fermi_ellipses_final['RAJ2000'])}')
+    
+    print(fermi_ellipses_final)
+    for i in range(len(fermi_ellipses_patches)):
+        fermi_plot(fermi_ellipses_patches[i], dict_keys[i], unique_sources_sp2, dict_values[i])
         
+            
 if __name__ == '__main__':
     main()
--- code/multiwavecataloging.py
+++ code/multiwavecataloging.py
@@ -6,15 +6,33 @@ from copy import copy
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 
+from astropy.table import Table, Column, MaskedColumn
+from astropy.table import vstack
+
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 import astropy.coordinates as coord
 from astropy.io import fits
 # from astropy.table import Table
+"""Adding ID as a required parameter for the catalog so that it can be matched to the full catalog and we regain all the info we previously lost!"""
+# idea for best guessing parameter call by doing a match case with some of its key words (but only accepts one column per parameter) 
+# would start class out to be with this best guess feature in which: (first make all uppercase .upper then) (ra: RA), (dec: DEC), (semimaj: MAJ),
+# (semiminor: MIN), (postion_angle: PA or POSANG or POSITIONANGLE) (ID: should just be any unique Identifier, ID, or SOURCE_NAME, NAME), 
+# (the fluxes & spec IDX might be a bit harder!)
 
-"""Todo: come up with a better way to get the required column values"""
+"""Todo:"""
+
+"""1. come up with a better way to get the required column values -- a dict call"""
 """2. if calling from the same catalog make a copy to avoid error pop up between css and uss"""
-"""3. add a print final pop up for the # of matched indexs"""
+"""3. add a print final pop up for the total # of matched indexs"""
+"""4. create a new line for the output images so they dont go off onto the screen, maybe just make smaller if the len is larger?"""
+# done - 6/12
+# """5. add a INDEX column without having to call for it so that it can reconstruct base catalog with minimal effort, 
+# I only call the index of the REDUCED CATALOG not really helpful in this case """  
+"""maybe this means changing the init params to take the full catalog, and a dict of the required params so im not doing this silly list thing like ra:BLANK dec:BLANK"""
+# done - 6/13
+# """6. fill the catalog right when you get it!"""
+
 # the point of this is just to reduce a catalog, right now
 # only for simple masking/filling and the radio features
 # but it can use the masking feature or filled feature to be a simple reduction
@@ -22,89 +40,92 @@ class Catalog:
     # assumes that it is already called from VizieR or is a table
     # assumes that the user reordered their columns in the order of :
     # ra, dec, semimajor, semiminor, flux, peak_flux, specindex (easy to reorder with astropy)
-    def __init__(self, catalog, catalog_labels, catalog_type):
+    def __init__(self, catalog, catalog_labels, catalog_type=None):
         # catalog type is between "radio", "infared", "optical" and "infared"
         # initalize:
         # catalog
         # catalog type
-        self.catalog = catalog
-        # no current utility 
+        try:
+            self.catalog = catalog.filled(np.nan)
+        except AttributeError:
+            self.catalog = catalog
+            
         self.type    = catalog_type
+        self.inital_size = len(self.catalog)
         # call these from the initalized catalog 
-        # these are only labels  
+        # these are only labels replace with dict
+    
         self.ra  = catalog_labels[0]
         self.dec = catalog_labels[1]
         self.semimajor = catalog_labels[2]
         self.semiminor = catalog_labels[3]
         self.positionangle = catalog_labels[4]
-                
         match self.type:
-            case 'uss':
+            case 'uss' | 'css':
+                '''changed for scode temp'''
                 # flux
                 self.flux = catalog_labels[5]
                 # peak_flux
                 self.peak_flux = catalog_labels[6]
                 # specindex
-                self.spec_index = catalog_labels[7]
-            case 'css':
-                # flux
-                self.flux = catalog_labels[5]
-                # peak_flux
-                self.peak_flux = catalog_labels[6]
-                # specindex
-                self.spec_index = catalog_labels[7]
+                self.spec_index = catalog_labels[7]  
+                # specindex error
+                self.spec_error = catalog_labels[8]
+                # scode
+                self.scode = catalog_labels[9]
+
+            case 'fermi':
+                ...
+                
             case _:
+
                 self.flux = None
                 self.peak_flux = None
                 self.spec_index = None
         
+        # add indexes list
+        if 'Indexes' in self.catalog.colnames:
+            pass
+        else:
+            indexes = list(range(len(self.catalog)))
+            self.catalog.add_column(indexes, name='Indexes')
     # have a intermediary reduction step?
-    
     # flux ratio 
     def flux_ratio(self):
         self.flux_ratio_ = self.catalog[self.peak_flux]/self.catalog[self.flux]
         self.catalog.add_column(self.flux_ratio_, name='FluxRatio')
     # baseline for all sources is that it is a compact source
     # and that none of the columns have empty spots 
-    
     # any catalog type
     # ---
     def robust(self):
-        self.catalog = self.catalog.filled(np.nan)
+        self.catalog = self.catalog
         return self.catalog
-    
     # radio only
     # -----
-    def uss(self, alpha_threshold=-1.4):
-        c_step_1 = self.catalog[self.catalog['FluxRatio'] <=1.2]
+    def radio_reduction(self, alpha_threshold=None):
+        c_step_1 = self.catalog[self.catalog['FluxRatio'] <=1.1]
         c_step_2 = c_step_1[c_step_1['FluxRatio'] >=0.8]
+        # testing s_code (the ngauss == 1 is the same)
+        c_step_3 = c_step_2[c_step_2[self.scode] =='S']
         # ensure spectral index exists
-        spec_idx_mask = np.isnan(c_step_2[self.spec_index].filled(np.nan))
-        c_step_3 = c_step_2[~spec_idx_mask]
+        spec_idx_mask = np.isnan(c_step_3[self.spec_index])
+        c_step_4 = c_step_3[~spec_idx_mask]
+        # remove all spectral index errors that are crazy!
+        c_step_5 = c_step_4[c_step_4[self.spec_error] >-999]
+
         # uss part
-        self.uss_sources = c_step_3[c_step_3[self.spec_index]< alpha_threshold]
+        match self.type:
+            case 'uss':
+                alpha_threshold = -1.4
+                self.catalog = c_step_5[c_step_5[self.spec_index]< alpha_threshold]
 
-        print(f'Sources removed: {len(self.catalog)-len(self.uss_sources)}')
-        print(f'{len(self.uss_sources)} USS candidates')
-        # overwrite the catalog
-        self.catalog = self.uss_sources.filled(0)
-        return self.catalog 
+            case 'css':
+                alpha_threshold = -1
+                self.catalog = c_step_5[c_step_5[self.spec_index]< alpha_threshold]
 
-    def css(self, alpha_threshold=-1):
-        c_step_1 = self.catalog[self.catalog['FluxRatio'] <=1.2]
-        c_step_2 = c_step_1[c_step_1['FluxRatio'] >=0.8]
-        # ensure spectral index exists
-        spec_idx_mask = np.isnan(c_step_2[self.spec_index].filled(np.nan))
-        c_step_3 = c_step_2[~spec_idx_mask]
-        # css part
-        self.css_sources = c_step_3[c_step_3[self.spec_index]< alpha_threshold]
-        # input angular size limiter
-
-        print(f'Sources removed: {len(self.catalog)-len(self.css_sources)}')
-        print(f'{len(self.css_sources)} USS candidates')
-        # overwrite catalog
-        self.catalog = self.css_sources.filled(0)
-        return self.catalog 
+        print(f'Sources removed: {self.inital_size-len(self.catalog)}')
+        print(f'{len(self.catalog)} {self.type} candidates')
         
 # the point of this is that we can have mutiple different objects == differnt overlays
 # a overlay ultimate goal is to create a list of indexes that match between the comparision
@@ -113,27 +134,34 @@ class Catalog:
 # we inherent the base catalogs ordered in the above way to make the calculations go quicker
 # that part requires user inputclass CatalogPositionOverlay(Catalogs):
 # calling base catalogs and comparision catalogs 
-# the base catalog should be a radio source or something with a high end to end emission
-# while fermi error ellipises 
+# the base catalog should be a radio source or something with a large area
+# for two catalogs only takes about ~2 minutes (with ~200k & 7195 sources)
 class CatalogOverlayer:
-    def __init__(self, base_catalog: Catalog, comparision_catalogs: Catalog): 
+    def __init__(self, base_catalog: Catalog, comparision_catalogs: Catalog, **kwargs): 
         self.base = base_catalog
         self.comparision = comparision_catalogs
-        # the number of catalogs being compared
+        # the number of catalogs being compared 
+        '''rename'''
         self.comparision_index = len(self.comparision)
+        # size scaling for capturing within the regions area
+        self.size_scale = kwargs.get('beam_scale', 1)
     
         # could make a attribute that follows the number of comparision catalogs
         for index in range(self.comparision_index):
             comparing_cat = self.comparision[index]
-            if comparing_cat.type == 'uss':
-                comparing_cat.flux_ratio()
-                comparing_cat.uss()
+            # in here we would then just call for the general dict reduction and it will be further curated by the keys in the dict
+            # change to is in keys in future
+            match comparing_cat.type:
+                case 'uss':
+                    comparing_cat.flux_ratio()
+                    comparing_cat.radio_reduction()
             
-            elif comparing_cat.type == 'css':
-                comparing_cat.flux_ratio()
-                comparing_cat.css()
-        
-        # this might take like 5+ min just for 2 cat and a base one?
+                case 'css':
+                    comparing_cat.flux_ratio()
+                    comparing_cat.radio_reduction()
+                case _:
+                    comparing_cat.robust()
+                    
         # check if in ellipse
         self.is_in_ellipse()
         # redefine ellipse into dict and other forms 
@@ -142,17 +170,22 @@ class CatalogOverlayer:
     # only need if the coordinate systems dont align, this should be a check to add in the ra and dec key word search
     def coordinate_matching(self):
         ...
-    # this is good for only one pairing 
+    # this is good for only one pairing  
     def is_in_ellipse(self):
         # only for ones who have the same coordinate system so be wary of that
         # a more friendly way to do this would be to make it into a dict so
         # there is less of an error in order
         # 1. decompose points
-        # kind of disgusting redo it (make into a four loop?)
+        # (make into a four loop?)
         ra_center_ellipses  = self.base.catalog[self.base.ra]
         dec_center_ellipses = self.base.catalog[self.base.dec]
-        semi_major_axis     = self.base.catalog[self.base.semimajor]
-        semi_minor_axis     = self.base.catalog[self.base.semiminor]
+        # maybe have to put .unit()
+        try: 
+            semi_major_axis     = self.base.catalog[self.base.semimajor].to(u.deg)*self.size_scale
+            semi_minor_axis     = self.base.catalog[self.base.semiminor].to(u.deg)*self.size_scale
+        except:
+            semi_major_axis     = self.base.catalog[self.base.semimajor]*u.deg*self.size_scale
+            semi_minor_axis     = self.base.catalog[self.base.semiminor]*u.deg*self.size_scale
         position_angle      = self.base.catalog[self.base.positionangle]
         
         for index in range(self.comparision_index):
@@ -180,7 +213,7 @@ class CatalogOverlayer:
                 y_rotated = x_distances_from_center * sin + y_distances_from_center *cos
                     
                 # 5.check if and y_rot are in the ellipse equation and make a append
-                radius = (x_rotated**2/(semi_major_axis[iteration]*u.deg)**2)+ ((y_rotated**2)/(semi_minor_axis[iteration]*u.deg)**2)       
+                radius = (x_rotated**2/(semi_major_axis[iteration])**2)+ ((y_rotated**2)/(semi_minor_axis[iteration])**2)       
                 
                 # 6. append radius of all mals sources to fermi ellipse [i]
                 self.comparision[index].radiuses_from_ellipse.append(radius)
@@ -192,17 +225,21 @@ class CatalogOverlayer:
     # creates a dict of the ellipse, pointing relationship, and a index for all the matched points
     # does this for all compared catalogs
     def ellipse_point_indexing(self):
+        # self.comparision_index and index is hard to read!
         for index in range(self.comparision_index):
             comparing_cat = self.comparision[index]
             
             major_indices = [ellipse_index for ellipse_index,
                              ellipse in enumerate(comparing_cat.is_in_list) if any(ellipse)]
             
-            # have a radiuses array include it?
+            # I have a radiuses array should I include it in output?
             # radiuses_array = np.array(radiuses_from_ellipse)[major_indices]
+            
+            # array of the ellipses that have atleast one point within it.
             filtered_list = np.array(comparing_cat.is_in_list)[major_indices]
             
             # change later there are better sub filter algos
+            # basic algo. to sort the ellipse index value in fermi catalog to the index of the point
             self.comparision[index].ellipse_point_index = {}
             self.comparision[index].point_all_indexes   = []
             self.comparision[index].ellipse_indexes     = major_indices
@@ -210,25 +247,31 @@ class CatalogOverlayer:
             for ellipse in filtered_list:
                 point_indices = []
                 index_num = 0 
+                # iterates to see if the index number is in the ellipse 
+                # Im going to change it so that it will be linked to the actual inital index so that no matter what the indexes can match
+                # that way the indexes that are held onto can correlate across similar catalogs easier!
                 for indice in ellipse:
                     if indice == True:
+                        # this will input the inital indexes to transfer across catalogs!
+                        # point_indices.append(comparing_cat.catalog['Indexes'][index_num])
                         point_indices.append(index_num)
+
                         self.comparision[index].point_all_indexes.append(index_num)
                         index_num +=1
                     else:
                         index_num +=1
+                        
                 self.comparision[index].ellipse_point_index[major_indices[j]] = point_indices
                 j+=1
         # return self.comparision
 
 # you asked for the matched catalogs back >:)
     def matched_catalogs(self):
-        # this is the issue it cant be a set the order is unique fix tomorrow
         all_matched_ellipses = []
         ellipse_point_dicts  = []
         reduced_compared_cats = []
         
-        # get indexes and reduces catalogs 
+        # get indexes and reduces catalogs to only be the matching ones 
         for index in range(self.comparision_index):
             ellipse_indexes = self.comparision[index].ellipse_indexes
             point_indexes   =  self.comparision[index].point_all_indexes
@@ -251,11 +294,12 @@ class CatalogOverlayer:
         # and the first catalog without the matching THIS IS TEMP
         return self.base_matched_catalog, self.comparision_matched_catalog, self.dict_match, self.comparision[0].catalog
     
-    
 # something to visualize the catalog overlay in different ways?
 class Multicatalogvisual():
     ...
 # make a function that will plt the values of the scatter in that region and the patch
+
+'''UPDATE TO CONSIDER THE INDEXES FROM THE BASE CATALOG!'''
 def fermi_plot(ellipse, ellipse_indexes, point_catalog, point_indexes):
     # can add the CSS_indexes later
     fig = plt.figure(figsize=(12,12))
@@ -264,8 +308,17 @@ def fermi_plot(ellipse, ellipse_indexes, point_catalog, point_indexes):
     new_ellipse = copy(ellipse)
     # get the ra and dec of the mal catalog
     # start the list here
-    ra = point_catalog[point_indexes]['RAJ2000']
-    dec = point_catalog[point_indexes]['DEJ2000']
+
+    # capture either iteration method!
+    try:
+        point_catalog = point_catalog[point_indexes]
+    except:
+    # it will iterate through all of them in the fermi plotting ?
+    # since these are unique per ellipse it wont be an issue?
+    # this should work even if I am not doing the mals spectral combining!
+        point_catalog = point_catalog[np.isin(point_catalog['Indexes'], point_indexes)]
+    ra = point_catalog['RAJ2000']
+    dec = point_catalog['DEJ2000']
     
     # ra reordering
     ra_coords = coord.Angle(ra)
@@ -296,7 +349,6 @@ def fermi_plot(ellipse, ellipse_indexes, point_catalog, point_indexes):
     plt.ylim(dec_ellipse-1, dec_ellipse+1)
 
     # uss scatter
-
     # plt area 
     anchored_text = AnchoredText(f'Ellipse Area: {area} \nID: {ellipse_indexes}', loc =2)
     ax3.add_artist(anchored_text)
@@ -307,3 +359,53 @@ def fermi_plot(ellipse, ellipse_indexes, point_catalog, point_indexes):
     plt.savefig('fermi_ellipse_example.jpg', dpi=300)
     plt.show()
     # change x ticks to degs or hours
+
+def spectral_window_merging(base_window, overlapping_window, matched_dict):
+  # indicies seperation
+  base_window_new = base_window.copy()
+  keys_list = list(matched_dict.keys())
+  comparision_spw_indices = []
+  for spw_index in keys_list:
+      comparision_spw_indices.append(matched_dict[spw_index][0])
+      
+  # call any index really
+  Spectral_window_type = overlapping_window['SPW/WB'][0]
+  column_length        = len(base_window)
+  empty_data           = np.zeros(column_length)
+  
+  # all the empty columns
+  flux      = MaskedColumn(empty_data, name = f'Flux_{Spectral_window_type}').filled(np.nan)
+  Peak_flux = MaskedColumn(empty_data, name = f'FluxPk_{Spectral_window_type}')
+  Spw_fit   = MaskedColumn(empty_data, name = f'Spwfit_{Spectral_window_type}')
+  Spw_efit  = MaskedColumn(empty_data, name = f'e_Spwfit_{Spectral_window_type}')
+
+  #   # get overlap for all indexes
+  #   overlap = np.zeros(len(base_window)+len(overlapping_window), dtype=bool)
+  #   overlap[comparision_spw_indices] = True
+      
+  mask = np.ones(len(overlapping_window), dtype=bool)
+  mask[comparision_spw_indices] = False
+  unique_sources = overlapping_window[mask] 
+  for index in range(len(keys_list)):
+      flux[keys_list[index]] = overlapping_window['Flux'][comparision_spw_indices[index]]
+      Peak_flux[keys_list[index]] = overlapping_window['FluxPk'][comparision_spw_indices[index]]
+      Spw_fit[keys_list[index]] = overlapping_window['Spwfit'][comparision_spw_indices[index]]
+      Spw_efit[keys_list[index]] = overlapping_window['e_Spwfit'][comparision_spw_indices[index]]
+# for some reason failed when i assigned it to a catalog, catalog calling issue.
+  try:
+      base_window_new.add_columns([flux,Peak_flux,Spw_fit,Spw_efit])
+  except:
+      base_window_new = base_window_new
+    #   print(f'Unique source catalog {unique_sources} ')
+  unique_detections = vstack([base_window_new, unique_sources], join_type='outer')
+  # unique_detections.add_column(overlap, name='Spw_overlap')
+  return unique_detections
+
+
+# I will make a function that will always convert the individual indexing to a 'global' indexing'!
+# just uses the dict comprehension, this allows the indexes to act as
+# connecting point between steps in catalog reduction if there are intermediary steps in mutiple catalog calls for same base!
+def global_index(dict, dict_catalog):
+    dict_global = {key: [dict_catalog['Indexes'][value] for value in value_list] 
+                   for key, value_list in dict.items()}
+    return dict_global
